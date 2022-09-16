2dd313087cca50431238a639f9405cbfdb6f2aba
Vitaly Medvedev <vmedvedev@evolutiongaming.com>
2022-09-16 13:39:08 +0300

    Improve wsHook script so it is completely compatible with WebSocket API

--- public/wsHook.js
+++ public/wsHook.js
@@ -48,54 +48,78 @@ var wsHook = {};
         wsHook.modifyUrl = modifyUrl;
     };
 
-    var _WS = WebSocket;
-    WebSocket = function (url, protocols) {
-        var WSObject;
-        url = wsHook.modifyUrl(url) || url;
-        this.url = url;
-        this.protocols = protocols;
-        if (!this.protocols) {
-            WSObject = new _WS(url);
-        } else {
-            WSObject = new _WS(url, protocols);
-        }
-
-        var _send = WSObject.send;
-        WSObject.send = function (data) {
-            arguments[0] = wsHook.before(data, WSObject.url, WSObject) || data;
-            _send.apply(this, arguments);
+    // __extends is a polyfill for class from TS playground
+    var __extends = (function () {
+        var extendStatics = function (d, b) {
+            extendStatics = Object.setPrototypeOf ||
+                ({__proto__: []} instanceof Array && function (d, b) {
+                    d.__proto__ = b;
+                }) ||
+                function (d, b) {
+                    for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+                };
+            return extendStatics(d, b);
         };
+        return function (d, b) {
+            extendStatics(d, b);
+            function __() {
+                this.constructor = d;
+            }
 
-        // Events needs to be proxied and bubbled down.
-        WSObject._addEventListener = WSObject.addEventListener;
-        WSObject.addEventListener = function () {
-            var eventThis = this;
-            // if eventName is 'message'
-            if (arguments[0] === "message") {
-                arguments[1] = (function (userFunc) {
-                    return function instrumentAddEventListener() {
+            d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+        };
+    })();
+    WebSocket = (function (_WS) {
+        __extends(WebSocketWrapper, _WS);
+        function WebSocketWrapper(url, protocols) {
+            var WSObject;
+            url = wsHook.modifyUrl(url) || url;
+            this.url = url;
+            this.protocols = protocols;
+            if (!this.protocols) {
+                WSObject = new _WS(url);
+            } else {
+                WSObject = new _WS(url, protocols);
+            }
+
+            var _send = WSObject.send;
+            WSObject.send = function (data) {
+                arguments[0] = wsHook.before(data, WSObject.url, WSObject) || data;
+                _send.apply(this, arguments);
+            };
+
+            // Events needs to be proxied and bubbled down.
+            WSObject._addEventListener = WSObject.addEventListener;
+            WSObject.addEventListener = function () {
+                var eventThis = this;
+                // if eventName is 'message'
+                if (arguments[0] === "message") {
+                    arguments[1] = (function (userFunc) {
+                        return function instrumentAddEventListener() {
+                            arguments[0] = wsHook.after(new MutableMessageEvent(arguments[0]), WSObject.url, WSObject);
+                            if (arguments[0] === null) return;
+                            userFunc.apply(eventThis, arguments);
+                        };
+                    })(arguments[1]);
+                }
+                return WSObject._addEventListener.apply(this, arguments);
+            };
+
+            Object.defineProperty(WSObject, "onmessage", {
+                "set": function () {
+                    var eventThis = this;
+                    var userFunc = arguments[0];
+                    var onMessageHandler = function () {
                         arguments[0] = wsHook.after(new MutableMessageEvent(arguments[0]), WSObject.url, WSObject);
                         if (arguments[0] === null) return;
                         userFunc.apply(eventThis, arguments);
                     };
-                })(arguments[1]);
-            }
-            return WSObject._addEventListener.apply(this, arguments);
-        };
+                    WSObject._addEventListener.apply(this, ["message", onMessageHandler, false]);
+                }
+            });
 
-        Object.defineProperty(WSObject, "onmessage", {
-            "set": function () {
-                var eventThis = this;
-                var userFunc = arguments[0];
-                var onMessageHandler = function () {
-                    arguments[0] = wsHook.after(new MutableMessageEvent(arguments[0]), WSObject.url, WSObject);
-                    if (arguments[0] === null) return;
-                    userFunc.apply(eventThis, arguments);
-                };
-                WSObject._addEventListener.apply(this, ["message", onMessageHandler, false]);
-            }
-        });
-
-        return WSObject;
-    };
+            return WSObject;
+        }
+        return WebSocketWrapper;
+    }(WebSocket));
 })();
