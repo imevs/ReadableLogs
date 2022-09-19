a9ff7024023f4abfa91929d5144f24402d1cae27
Vitaly Medvedev <vmedvedev@evolutiongaming.com>
2022-09-20 00:22:38 +0300

    Add yaml option to demo page

--- src/demo.ts
+++ src/demo.ts
@@ -12,13 +12,21 @@ function executeFormatter(data: typeof logs) {
     console.log("JSON message 2", data.current);
 
     const multiline = !!(document.querySelector("#multiline") as HTMLInputElement)?.checked ?? false;
+    const yaml = !!(document.querySelector("#yaml") as HTMLInputElement)?.checked ?? false;
 
-    const result = parseMessage(data.current, { showDiffWithObject: data.prevObject, multiline: multiline });
+    const result = parseMessage(
+        data.current,
+        yaml ? { yaml: true } : { showDiffWithObject: data.prevObject, multiline: multiline },
+    );
     console.info(...formatForLoggingInBrowser("Formatted message 2: ", result));
 
-    document.querySelector("#demo_input_currentMessage")!.innerHTML = highlightTextInHtml(result);
-    document.querySelector("#demo_input_prevMessage")!.innerHTML = data.prevObject ?
-        formatMultiLineTextAsHTML(JSON.stringify(data.prevObject, null, "  ")) : "";
+    if (yaml) {
+        document.querySelector("#demo_input_prevMessage")!.innerHTML = highlightTextInHtml(result);
+    } else {
+        document.querySelector("#demo_input_currentMessage")!.innerHTML = highlightTextInHtml(result);
+        document.querySelector("#demo_input_prevMessage")!.innerHTML = data.prevObject ?
+            formatMultiLineTextAsHTML(JSON.stringify(data.prevObject, null, "  ")) : "";
+    }
 }
 
 function onClick() {
@@ -40,5 +48,5 @@ function onClick() {
 
 executeFormatter(logs);
 
-document.querySelector("#run")!.addEventListener("click", () => { onClick(); });
-document.querySelector("#multiline")!.addEventListener("click", () => { onClick(); });
+["#run", "#yaml", "#multiline"].map(selector =>
+    document.querySelector(selector)!.addEventListener("click", onClick));
