01d74eb4fdf2472fb23c9e7e105bfa0bbdb09456
Vitaly Medvedev <vmedvedev@evolutiongaming.com>
2022-10-07 23:38:29 +0300

    Allow specifying styles for log parts and add an annotation to explain meaning of colors

--- src/formattingUtils.ts
+++ src/formattingUtils.ts
@@ -1,7 +1,6 @@
 import { FormattingType, LogItem } from "./types";
 
 type Hex = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "A" | "B" | "C" | "D" | "E" | "F";
-type Color = "red" | "blue" | "pink" | "orange" | "green" | "lightgreen" | "" | `#${string}`;
 
 type ValidateHex<T extends Hex> = T;
 // @ts-expect-error "is declared but its value is never read."
@@ -11,22 +10,22 @@ function color<T extends string>(s: ValidateColor<T>): T {
     return s;
 }
 
-export const typeToColorMap: Record<FormattingType, Color> = {
+export const typeToColorMap: Record<FormattingType, string> = {
     unknown: "",
     specialSymbols: "",
-    value: "lightgreen",
-    string: color("#008000"),
-    number: color("#0000FF"),
-    boolean: color("#000080"),
-    key: color("#660E6A"),
-    added: "blue",
-    changed: "green",
-    removed: "red",
-    error: "red",
-    annotation: color("#808080"),
+    value: "color: " + "lightgreen",
+    string: "color: " + color("#008000"),
+    number: "color: " + color("#0000FF"),
+    boolean: "color: " + color("#08FFF5"),
+    key: "color: " + color("#660E6A"),
+    added: "color: " + "blue;background: grey",
+    changed: "color: " + "lightgreen;text-decoration: underline",
+    removed: "color: " + "red",
+    error: "color: " + "red",
+    annotation: "color: " + color("#808080"),
 };
 
-function getColor(type: FormattingType): Color {
+function getStyle(type: FormattingType): string {
     return typeToColorMap[type];
 }
 
@@ -49,10 +48,35 @@ export function formatForLoggingInBrowser(
         removedItems.map(item => item.path + ":" + item.text)
             .join(",").split("/").join("."), // replace "/" path separator as it is treated by dev tools as part of url
         ...prefixColors,
-        ...notRemovedItems.map(item => item.type !== "specialSymbols" ? `color: ${getStyle(item.type)};` : ""),
-        ...(removedItems.length > 0 ? ["", `color: ${getStyle("removed")};`] : [])
+        ...notRemovedItems.map(item => item.type !== "specialSymbols" ? `${getStyle(item.type)};` : ""),
+        ...(removedItems.length > 0 ? ["", `${getStyle("removed")};`] : [])
     ];
 }
+
+function showLegendForSyntaxHighlighting() {
+    console.log(...formatForLoggingInBrowser("", [
+        {
+            type: "annotation",
+            path: "root",
+            text: "You are using ReadableLogs library, it sets specific styles to make logs more readable (you can specify your own colors in configuration):\n "
+        },
+        { type: "added", path: "root", text: "Added text" },
+        { type: "annotation", path: "root", text: "   " },
+        { type: "changed", path: "root", text: "Changed text" },
+        { type: "annotation", path: "root", text: "   " },
+        { type: "key", path: "root", text: '"keyOfObject"' },
+        { type: "annotation", path: "root", text: "   String: " },
+        { type: "string", path: "root", text: '"String of text"' },
+        { type: "annotation", path: "root", text: "   Number: " },
+        { type: "number", path: "root", text: "1234.567" },
+        { type: "annotation", path: "root", text: "   Boolean: " },
+        { type: "boolean", path: "root", text: "false" },
+        { type: "annotation", path: "root", text: "   " },
+        { type: "removed", path: "root", text: "removed text" }
+    ]));
+}
+
+showLegendForSyntaxHighlighting();
 
 export function formatMultiLineTextAsHTML(content: string): string {
     return content.split(" ").join("&nbsp;").split("\n").join("<br />");
@@ -68,7 +92,7 @@ export function highlightTextInHtml(messages: LogItem[] | LogItem[][]): string {
     const formattedMessages = ((Array.isArray(messages[0]) ? messages : [messages]) as LogItem[][]).map(message => {
         return message
             .filter(part => part.type !== "removed") /* TODO: show removed part separately to do not break structure of JSON */
-            .map(part => `<span style="color: ${getColor(part.type)}">${formatMultiLineTextAsHTML(part.text)}</span>`)
+            .map(part => `<span style="${getStyle(part.type)};">${formatMultiLineTextAsHTML(part.text)}</span>`)
             .join("");
     });
     if (!Array.isArray(messages[0])) {
